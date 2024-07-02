e4dfa278d6a3d1904b260c3eaa44f47f73183316
Mauricio Chimento <jmau.chimento@gmail.com>
2024-07-02 11:28:01 +0200

    update services manner to display description

--- assets/js/toggleContent.js
+++ assets/js/toggleContent.js
@@ -1,18 +1,18 @@
 
-function toggleContent(button) {
-  const content = button.parentElement.nextElementSibling;
-  content.classList.toggle('show');
-  
-  const plusIcon = button.querySelector('.fa-plus');
-  const minusIcon = button.querySelector('.fa-minus');
-  
-  if (content.classList.contains('show')) {
-    plusIcon.style.display = 'none';
-    minusIcon.style.display = 'inline';
+function toggleContent(element, whoami) {
+  const content = element.querySelector(whoami);
+  const checkbox = element.querySelector('input[type="checkbox"]');
+
+  if (content.style.maxHeight === "0px" || content.style.maxHeight === "") {
     content.style.maxHeight = content.scrollHeight + "px";
+    checkbox.checked = true;
   } else {
-    plusIcon.style.display = 'inline';
-    minusIcon.style.display = 'none';
     content.style.maxHeight = "0";
+    checkbox.checked = false;
   }
+}
+
+function handleCheckboxChange(checkbox, whoami) {
+    const serviceItem = checkbox.closest('.service_item');
+    toggleContent(serviceItem, whoami);
 }
